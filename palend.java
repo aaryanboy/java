5b0f699d1ab7ae38c66c06f80d7c0e4597941c09
aaryanboy <aaryanboy12@gmail.com>
2023-10-02 10:33:51 +0545

    palendeone added

--- palend.java
+++ palend.java
@@ -3,6 +3,12 @@ public class palend {
     public static void main(String[] args){
         Solution e=new Solution();
         e.isArmstrong(151);
+        if (e.isPalendrone(101)==true) {
+            System.out.println("palendrone");
+        } else {
+             System.out.println("not palendrone");
+        }
+       
        
     }
 }
