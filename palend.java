14d278e9dcb4c83427a99df6d7ab30c9f7643651
aaryanboy <59251969+aaryanboy@users.noreply.github.com>
2023-10-02 10:41:06 +0545

    update from github

--- palend.java
+++ palend.java
@@ -3,11 +3,13 @@ public class palend {
     public static void main(String[] args){
         Solution e=new Solution();
         e.isArmstrong(151);
-        if (e.isPalendrone(101)==true) {
+        if (e.isPalendrone(201)==true) {
             System.out.println("palendrone");
         } else {
              System.out.println("not palendrone");
         }
+
+        
        
        
     }
