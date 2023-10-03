d50a748749f25ca022d40bed07d0d66a5251e2aa
aaryanboy <aaryanboy12@gmail.com>
2023-10-03 10:16:27 +0545

    sum of two numbers

---
+++ Takinginput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Takinginput {
+    public static void main(String[] args){
+    int a,b;
+    System.out.println("enter a num1");
+    Scanner alt= new Scanner(System.in);
+    a=alt.nextInt();
+    System.out.println("enter num2");
+    b=alt.nextInt();
+    System.out.println(a+b);
+}
+}
